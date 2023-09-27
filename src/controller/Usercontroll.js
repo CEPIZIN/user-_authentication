482594a8d887432be92b08237c6c892d755f2924
CEPIZIN <lauramathias03@gmail.com>
2023-09-27 18:34:34 -0300

    Update Usercontroll.js

--- src/controller/Usercontroll.js
+++ src/controller/Usercontroll.js
@@ -1,4 +1,5 @@
 import UserModel from "../model/user.js";
+import bcrypt from 'bcrypt';
 import validator from 'validator';
 
 class UserControll {
@@ -15,12 +16,26 @@ class UserControll {
             if (!validator.isEmail(email)) errors.push("Please enter a valid Email");
             if (!password) errors.push("Password is required");
             if (password !== confirmPassword) errors.push("Passwords do not match");
+
+            const userExist = await User.findOnde({email:email})
+            
+            if(userExist){
+                errors.push('User already exist')
+
+            const salt = await bcrypt.genSalt(12)
+            const passwordHash= await bcrypt.hash(password,salt)
+            }
     
             if (errors.length > 0) {
                 return res.status(422).json({ errors });
             }
     
-            const newUser = new UserModel({ name, email, password });
+            const newUser = new UserModel({
+                 name, 
+                 email, 
+                 password 
+                });
+
             const savedUser = await newUser.save();
         
             return res.status(201).json({
@@ -32,44 +47,52 @@ class UserControll {
             return res.status(500).send(`Error: ${error.message} - User registration failure`);
         }
     }
-    // user login 
-
+     
+    //User login 
     static login = async (req, res) => {
         try {
             const { email, password } = req.body;
-
+            const errors = [];
+    
             //validation 
             if (!email) errors.push("Email is required");
             if (!validator.isEmail(email)) errors.push("Please enter a valid Email");
             if (!password) errors.push("Password is required");
-
-            //check if user existi 
-            const user = await user.findOne({email:email})
-            if( !user){
-                return res.status(404).json({msg: 'User not found'})
+    
+            if (errors.length > 0) {
+                return res.status(400).json({ errors });
+            }
+    
+            //check if user exists 
+            const user = await UserModel.findOne({ email: email });
+            if (!user) {
+                return res.status(404).json({ msg: 'User not found' });
+            }
+    
+            //check if password is valid 
+            const checkPassword = await bcrypt.compare(password, user.password);
+            if (!checkPassword) {
+                return res.status(401).json({ msg: 'Invalid password' });
             }
 
-            //check id password exist 
-            const checkPassword = await bcrypt.compare(password, user.password)
-            if(!password){
-                return res.status(404).json({msg: 'Invalid password '})
-            }
-            
-            try{
-                const secret = process.env.SECRET
-                const token =  jwt.sing({
-                    id: user._id
-                }, secret,
+            try {
+                const secret = process.env.SECRET;
+                const token = jwt.sign(
+                    {
+                    id: user._id,
+                    }, 
+                    secret,
                 )
-                   res.status(200).json({msg:'Authentication successful',token})                                                 
-
-            } catch(error){
-                console.log(error)
-                res.ststus(500).json({
-                    msg: '"A server error occurred, please try again later'
-                })
+    
+                res.status(200).json({ msg: 'Authentication successful', token });
+    
+            } catch (error) {
+                console.log(error);
+                res.status(500).json({
+                    msg: 'A server error occurred, please try again later'
+                });
             }
-
+    
         } catch (error) {
             return res.status(500).send(`Error: ${error.message} - User login failure`);
         }
