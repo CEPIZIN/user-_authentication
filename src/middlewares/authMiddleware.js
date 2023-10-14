ed8a44a8f07a8ae24e9cf987f553a8f6f492059d
CEPIZIN <lauramathias03@gmail.com>
2023-10-13 21:03:09 -0300

    update middlewar

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,5 +3,10 @@ import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 dotenv.config()
 
+const verifyToken =  (req,res,next) =>{
+    const tokne = req.header
+
+}
+
 
 const secret = process.env.SECRET;
